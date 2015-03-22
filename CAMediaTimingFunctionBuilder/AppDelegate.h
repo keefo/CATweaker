2ff353f7af28584abdce0acc71c20ee360b1522d
Xu Lian <lianxu@beyondcow.com>
2015-03-22 10:17:12 -0700

    added function generator

--- CAMediaTimingFunctionBuilder/AppDelegate.h
+++ CAMediaTimingFunctionBuilder/AppDelegate.h
@@ -11,6 +11,7 @@
 
 @interface AppDelegate : NSObject <NSApplicationDelegate>
 @property(assign) IBOutlet CurveView *curveView;
+@property(assign) IBOutlet NSTextView *textView;
 
 @end
 
--- CAMediaTimingFunctionBuilder/AppDelegate.m
+++ CAMediaTimingFunctionBuilder/AppDelegate.m
@@ -12,7 +12,6 @@
 
 
 @interface AppDelegate ()
-
 @property (weak) IBOutlet NSWindow *window;
 @end
 
@@ -51,6 +50,8 @@
            selector:@selector(pointChangeNotification:)
                name:@"PointChangeNotification"
              object:nil];
+    
+    [self pointChangeNotification:nil];
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
@@ -75,8 +76,27 @@
 {
     NSPoint p1 = _curveView.point1;
     NSPoint p2 = _curveView.point2;
-    
     NSLog(@"%@ %@", NSStringFromPoint(p1), NSStringFromPoint(p2));
+    
+    NSDictionary *keywordAttribute = @{ NSForegroundColorAttributeName : [NSColor colorWithDeviceRed:0.419 green:0.124 blue:0.684 alpha:1.000]};
+    NSDictionary *textAttribute = @{ NSForegroundColorAttributeName : [NSColor blackColor]};
+    NSDictionary *numberAttribute = @{ NSForegroundColorAttributeName : [NSColor colorWithDeviceRed:0.000 green:0.195 blue:0.990 alpha:1.000]};
+    
+    NSMutableAttributedString *str = [[NSMutableAttributedString alloc] initWithString:@"CAMediaTimingFunction" attributes:keywordAttribute];
+    [str appendAttributedString:[[NSAttributedString alloc] initWithString:@" *timingFunction = [" attributes:textAttribute]];
+    [str appendAttributedString:[[NSAttributedString alloc] initWithString:@"CAMediaTimingFunction " attributes:keywordAttribute]];
+    [str appendAttributedString:[[NSAttributedString alloc] initWithString:@"functionWithControlPoints: " attributes:textAttribute]];
+    [str appendAttributedString:[[NSAttributedString alloc] initWithString:[NSString stringWithFormat:@"%.6f", p1.x] attributes:numberAttribute]];
+    [str appendAttributedString:[[NSAttributedString alloc] initWithString:@" : " attributes:textAttribute]];
+    [str appendAttributedString:[[NSAttributedString alloc] initWithString:[NSString stringWithFormat:@"%.6f", p1.y] attributes:numberAttribute]];
+    [str appendAttributedString:[[NSAttributedString alloc] initWithString:@" : " attributes:textAttribute]];
+    [str appendAttributedString:[[NSAttributedString alloc] initWithString:[NSString stringWithFormat:@"%.6f", p2.x] attributes:numberAttribute]];
+    [str appendAttributedString:[[NSAttributedString alloc] initWithString:@" : " attributes:textAttribute]];
+    [str appendAttributedString:[[NSAttributedString alloc] initWithString:[NSString stringWithFormat:@"%.6f", p2.y] attributes:numberAttribute]];
+    [str appendAttributedString:[[NSAttributedString alloc] initWithString:@"];" attributes:textAttribute]];
+
+    //[CAMediaTimingFunction functionWithControlPoints:0.1 : 0.8 : 0.2 : 0.8]
+    [_textView.textStorage setAttributedString:str];
 }
 
 
