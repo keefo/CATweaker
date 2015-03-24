64b290e69482ee327c6287378126e010dc1c4e99
Xu Lian <lianxu@beyondcow.com>
2015-03-23 22:23:52 -0700

    added CATweakerTests

---
+++ CATweakerTests/CATweakerTests.m
@@ -0,0 +1,41 @@
+//
+//  CATweakerTests.m
+//  CATweakerTests
+//
+//  Created by X on 2015-03-21.
+//  Copyright (c) 2015 Beyondcow. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <XCTest/XCTest.h>
+
+@interface CATweakerTests : XCTestCase
+
+@end
+
+@implementation CATweakerTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testTimingFunctionWithNameRegex {
+    
+    // testing TimingFunctionWithNameRegex.
+
+}
+
+//- (void)testPerformanceExample {
+//    // This is an example of a performance test case.
+//    [self measureBlock:^{
+//        // Put the code you want to measure the time of here.
+//    }];
+//}
+
+@end
